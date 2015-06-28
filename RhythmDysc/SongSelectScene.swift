6f66859b29d650d8ebcdacf76a98c3883c9dc8e6
Terrence Park <trex@parkvue.com>
2015-06-27 19:51:39 -0500

    Added a dragging function to the song select menu + Created song selections of the same song for testing purposes

--- RhythmDysc/SongSelectScene.swift
+++ RhythmDysc/SongSelectScene.swift
@@ -16,7 +16,9 @@ class SongSelectScene: SKScene {
     var currSelection: SongSelection?;
     var selections: [SongSelection] = [SongSelection]();
     var touchedNode: SKNode?;
-    var touchDistanceMoved: Double = 0;
+    var startTouchLocation: CGPoint?;
+    var isMovingByTouch: Bool = false;
+    var selectionVelocity: CGFloat = 0;
     
     override init(size: CGSize) {
         selectionHeight = size.height/7;
@@ -31,10 +33,13 @@ class SongSelectScene: SKScene {
     }
     
     override func didMoveToView(view: SKView) {
-        backgroundColor = UIColor.whiteColor();
-        let testSelection = SongSelection(size: CGSize(width: size.width, height: selectionHeight), title: "aLIEz", artist: "SawanoHiroyuki[nZk]:mizuki", maxBPM: 99);
-        selections.append(testSelection);
-        addChild(testSelection);
+        backgroundColor = UIColor.blackColor();
+        for (var i = 0; i < 20; i++) {
+            let testSelection = SongSelection(size: CGSize(width: size.width, height: selectionHeight), title: "aLIEz", artist: "SawanoHiroyuki[nZk]:mizuki", maxBPM: 99);
+            testSelection.position.y = selectionHeight * CGFloat(i);
+            selections.append(testSelection);
+            addChild(testSelection);
+        }
         addChild(songDetailWindow);
     }
     
@@ -43,14 +48,25 @@ class SongSelectScene: SKScene {
             userInteractionEnabled = true;
             currSelection = nil;
         }
+        if (!isMovingByTouch) {
+            if (selectionVelocity > 0) {
+                selectionVelocity = min(selectionVelocity, selectionHeight-selections.first!.position.y);
+            } else {
+                selectionVelocity = max(selectionVelocity, size.height-selectionHeight*2-selections.last!.position.y);
+            }
+            for sel in selections {
+                sel.position.y += selectionVelocity;
+            }
+        }
+        selectionVelocity = selectionVelocity * 9 / 10;
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         let touch = touches.first as! UITouch;
-        let touchLocation = touch.locationInNode(self);
+        startTouchLocation = touch.locationInNode(self);
         if (currSelection == nil) {
             for selectionNode in selections {
-                if (selectionNode.containsPoint(touchLocation)) {
+                if (selectionNode.containsPoint(startTouchLocation!)) {
                     touchedNode = selectionNode;
                 }
             }
@@ -59,25 +75,44 @@ class SongSelectScene: SKScene {
     
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         let touch = touches.first as! UITouch;
-        let touchLocation = touch.locationInNode(self);
+        let currTouchLocation = touch.locationInNode(self);
         let prevTouchLocation = touch.previousLocationInNode(self);
-        if (sqrt(pow(touchLocation.x - prevTouchLocation.x, 2) + pow(touchLocation.y - prevTouchLocation.y, 2)) > 5) {
-            NSLog("Setting touchedNode to nil");
-            touchedNode = nil;
+        if (startTouchLocation != nil) {
+            let dist = hypot(startTouchLocation!.x - currTouchLocation.x, startTouchLocation!.y - currTouchLocation.y);
+            if (dist > 10 || isMovingByTouch) {
+                isMovingByTouch = true;
+                NSLog("Setting touchedNode to nil");
+                touchedNode = nil;
+                
+                let touchYChange = currTouchLocation.y - touch.previousLocationInNode(self).y;
+                let yChange: CGFloat;
+                if (touchYChange > 0) {
+                    yChange = min(touchYChange, selectionHeight-selections.first!.position.y);
+                } else {
+                    yChange = max(touchYChange, size.height-selectionHeight*2-selections.last!.position.y);
+                }
+                selectionVelocity = yChange;
+                for sel in selections {
+                    sel.position.y += yChange;
+                }
+            }
         }
     }
     
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         let touch = touches.first as! UITouch;
-        let touchLocation = touch.locationInNode(self);
+        let currTouchLocation = touch.locationInNode(self);
+        let prevTouchLocation = touch.previousLocationInNode(self);
         if (currSelection == nil) {
             for selectionNode in selections {
-                if (selectionNode.containsPoint(touchLocation) && touchedNode != nil) {
+                if (selectionNode.containsPoint(currTouchLocation) && touchedNode != nil) {
                     currSelection = touchedNode as? SongSelection;
                     showSongDetailWindow();
                 }
             }
         }
+        startTouchLocation = nil;
+        isMovingByTouch = false;
     }
     
     private func showSongDetailWindow() {
